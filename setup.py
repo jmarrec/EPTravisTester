aa906d230b894088fa248319ea4eed946e4a3107
Edwin Lee <leeed2001@gmail.com>
2020-03-26 14:42:17 -0500

    Allow it to throw exceptions so setup.py will fail correctly

--- setup.py
+++ setup.py
@@ -2,7 +2,6 @@ import distutils.cmd
 import distutils.log
 from setuptools import setup
 from ep_testing.downloader import Downloader
-from ep_testing.exceptions import EPTestingException
 from ep_testing.tester import Tester
 
 
@@ -24,15 +23,11 @@ class Runner(distutils.cmd.Command):
     def run(self):
         """Run command."""
         self.announce('Attempting to test tag name: %s' % TAG_NAME, level=distutils.log.INFO)
-        try:
-            d = Downloader(TAG_NAME, '/tmp', self.announce)
-            self.announce(d.extracted_install_path(), level=distutils.log.INFO)
-            t = Tester(d.extracted_install_path())
-            t.run()
-        except EPTestingException as e:
-            self.announce('Error occurred with an EnergyPlusTesting Exception: ' + str(e), level=distutils.log.ERROR)
-        except Exception as e:
-            self.announce('Error occurred with an UNKNOWN Exception: ' + str(e), level=distutils.log.ERROR)
+        d = Downloader(TAG_NAME, '/tmp', self.announce)
+        self.announce(d.extracted_install_path(), level=distutils.log.INFO)
+        t = Tester(d.extracted_install_path())
+        # unhandled exceptions should cause this to fail
+        t.run()
 
 
 setup(
