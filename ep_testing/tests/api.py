7b40cc4882dfd424eb5d2b7abc89ff0762e0fe58
Edwin Lee <leeed2001@gmail.com>
2020-03-27 07:46:06 -0500

    Try just making API script executable

--- ep_testing/tests/api.py
+++ ep_testing/tests/api.py
@@ -1,5 +1,5 @@
 import os
-import platform
+import stat
 from tempfile import mkstemp
 from subprocess import check_call, CalledProcessError  # , STDOUT
 
@@ -15,6 +15,7 @@ class TestPythonAPIAccess(BaseTest):
     @staticmethod
     def _api_script_content() -> str:
         return """
+#!/usr/bin/env python3
 from pyenergyplus.api import EnergyPlusAPI
 api = EnergyPlusAPI()
 glycol = api.functional.glycol(u"water")
@@ -28,16 +29,12 @@ for t in [5.0, 15.0, 25.0]:
         handle, python_file_path = mkstemp(suffix='.py')
         with os.fdopen(handle, 'w') as f:
             f.write(self._api_script_content())
+        st = os.stat(python_file_path)
+        os.chmod(python_file_path, st.st_mode | stat.S_IEXEC)
         print(' [FILE WRITTEN] ', end='')
         # dev_null = open(os.devnull, 'w')
         try:
-            if platform.system() == 'Linux':
-                py = 'python3'
-            elif platform.system() == 'Darwin':
-                py = 'python3'
-            else:  # windows
-                py = '/c/Python36/python'
-            check_call([py, python_file_path], env={'PYTHONPATH': install_root})  # , stdout=dev_null, stderr=STDOUT)
+            check_call([python_file_path], env={'PYTHONPATH': install_root})  # , stdout=dev_null, stderr=STDOUT)
             print(' [DONE]!')
         except CalledProcessError:
             raise EPTestingException('Python API Wrapper Script failed!')
