0861d415e60115e41af13832c5186844785f97bb
Edwin Lee <leeed2001@gmail.com>
2020-06-10 20:38:04 -0500

    Try tag2

--- ep_testing/config.py
+++ ep_testing/config.py
@@ -1,4 +1,6 @@
 import os
+import platform
+import shutil
 from tempfile import mkdtemp
 
 
@@ -6,19 +8,23 @@ class TestConfiguration:
 
     def __init__(self):
         self.this_version = '9.4'
-        self.tag_this_version = 'v9.4.0-TestSymlinkFix'
+        self.tag_this_version = 'v9.4.0-TestSymlinkFix2'
         self.last_version = '9.3'
         self.tag_last_version = 'v9.3.0'
 
         # If this is turned on, it expects to find an asset named target_file_name in the download_dir
         self.skip_download = True
-        self.skipped_download_dir = '/tmp/'
+        self.skipped_download_file = '/eplus/repos/6eplus/builds/r/EnergyPlus-9.4.0-7def4cec5e-Linux-x86_64.tar.gz'
 
         # But if we are on Travis, we override it to always download a new asset
         if os.environ.get('TRAVIS'):
             self.skip_download = False
 
+        self.download_dir = mkdtemp()
         if self.skip_download:
-            self.download_dir = self.skipped_download_dir
-        else:
-            self.download_dir = mkdtemp()
+            this_platform = platform.system()
+            target_file_name = 'ep.tar.gz'
+            if this_platform == 'Windows':
+                target_file_name = 'ep.zip'
+            file_path = os.path.join(self.download_dir, target_file_name)
+            shutil.copy(self.skipped_download_file, file_path)
