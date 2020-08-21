dd5cb97f2d3101f11cb238afee96dbb2e4365ddb
Edwin Lee <leeed2001@gmail.com>
2020-08-21 13:24:19 -0500

    Forgot one versbose thing to turn off

--- ep_testing/tests/api.py
+++ ep_testing/tests/api.py
@@ -51,8 +51,6 @@ for t in [5.0, 15.0, 25.0]:
                 py = 'python3'
             elif platform.system() == 'Darwin':
                 py = '/usr/local/bin/python3'
-                with open(python_file_path) as f:
-                    print(f.read())
             else:  # windows
                 py = 'C:\\Python36\\Python.exe'
             my_check_call(self.verbose, [py, python_file_path])
